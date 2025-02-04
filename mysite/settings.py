f64fae19dc48f797a105648afc76d1e12013c749
Kz1232 <kjdj802@gamil.com>
2025-02-04 22:16:42 +0545

    added the myapss in installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapps.apps.MyappsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
