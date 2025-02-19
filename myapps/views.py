b2a257c46911fae674cc854c6fbce3cc987f2c37
Kz1232 <kjdj802@gamil.com>
2025-02-19 12:27:06 +0545

    created loginform,views and route

--- myapps/views.py
+++ myapps/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth import login , authenticate
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
+@login_required
+def indexView(request):
+    activeUser=request.user
+    context={"user":activeUser}
+    return render(request ,"myapps/index.html",context)
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = None
+    if request.method == 'POST':
+        form =forms.LoginForm(request.POST or None)
+        if form.is_valid():
+            user =authenticate(
+                username=form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request,user)
+                message = f'Hello{user.username}! Logged in successfull'
+            else:
+                message = "Login failed"
+    return render(request,'authentication/login.html',context={"form":form,"message":message})
+
+
