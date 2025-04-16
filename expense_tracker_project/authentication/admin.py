08412c81c0d92ecca2e54fc4b201ee116b5f7647
Kshitiz <kjdj802@gmail.com>
2025-04-16 17:29:29 +0545

    added the custom configuration for admin on how to display the user related data in admin field

--- expense_tracker_project/authentication/admin.py
+++ expense_tracker_project/authentication/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ['email', 'is_staff','is_superuser',]
+    search_fields = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2','is_staff'),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, CustomUserAdmin)
+
