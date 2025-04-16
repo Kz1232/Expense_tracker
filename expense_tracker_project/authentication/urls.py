b060aa14c872f132aab4b935f1e421896ccb87ae
Kshitiz <kjdj802@gmail.com>
2025-04-16 18:06:30 +0545

    Added the route for login, create a template added the backend to process login.

--- expense_tracker_project/authentication/urls.py
+++ expense_tracker_project/authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'authentication'
 urlpatterns =[
-    path('',views.testview,name='testview'),
+    # path('',views.testview,name='testview'),
+    path('login/',views.login_view,name="login"),
 ]
--- expense_tracker_project/authentication/views.py
+++ expense_tracker_project/authentication/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate , login
 from django.http import HttpResponse
+from django.urls import reverse
+from .forms import UserForm
+
+
 # Create your views here.
-def testview(request):
-    # return HttpResponse("This is inside the authenticate apps")
-    return render(request ,"authentication/login.html")
+def login_view(request):
+    form = UserForm()
+    if request.method == 'POST':
+        email = request.POST['email']
+        password =request.POST['password']
+        user = authenticate(request,email = email , password = password) 
+        if user is not None:
+            login(request,user)
+            return redirect(reverse('myapps:homepage'))
+
+
+
+    context={'form':form}
+    return render(request ,"authentication/login.html",context)
